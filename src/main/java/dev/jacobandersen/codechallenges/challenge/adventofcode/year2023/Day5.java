97f9791173ad2cc787f82b73efe28ec93b3ad1f0
Jacob Andersen <19645494+simpleauthority@users.noreply.github.com>
2023-12-04 21:27:25 -0800

    start day5

---
+++ src/main/java/dev/jacobandersen/codechallenges/challenge/adventofcode/year2023/Day5.java
@@ -0,0 +1,19 @@
+package dev.jacobandersen.codechallenges.challenge.adventofcode.year2023;
+
+import dev.jacobandersen.codechallenges.challenge.adventofcode.Day;
+
+public class Day5 extends Day {
+    public Day5() {
+        super(2023, 5, "If You Give A Seed A Fertilizer");
+    }
+
+    @Override
+    public String partOne() {
+        return null;
+    }
+
+    @Override
+    public String partTwo() {
+        return null;
+    }
+}
